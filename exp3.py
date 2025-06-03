d9c1592898e57a15ba117a52b10715f11d933490
ujwal0713 <ujdev007@gmail.com>
2025-06-03 21:31:55 +0530

    .

--- exp3.py
+++ exp3.py
@@ -3,16 +3,21 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+
 iris = load_iris()
 X = iris.data 
 y = iris.target 
 label_names = iris.target_names
+
 df = pd.DataFrame(X, columns=iris.feature_names)
+df.head()
+
 pca = PCA(n_components=2)
 principal_components = pca.fit_transform(X)
-df_pca = pd.DataFrame(data=principal_components,
-columns=['Principal Component 1', 'Principal Component 2'])
+
+df_pca = pd.DataFrame(data=principal_components,columns=['Principal Component 1', 'Principal Component 2'])
 df_pca['Target'] = y
+
 plt.figure(figsize=(6, 4))
 colors = ['y', 'b', 'g']
 for i, label in enumerate(np.unique(y)):
